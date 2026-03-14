10482e79ef182c0d5fe33514fb3d2bcdae4ec833
Gakusara <4444.w.d.gaster@gmail.com>
2026-03-14 16:36:56 +0800

    individual content added

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -22,15 +22,41 @@ export const creators: Creator[] = [
   {
     name: 'Karasu', // 替换为你的姓名或昵称
     avatar: '', // 留空以使用 GitHub 头像，或提供头像 URL
-    username: '你的GitHub用户名', // 替换为你的实际 GitHub 用户名
+    username: 'Gakusara', // 替换为你的实际 GitHub 用户名
     title: '知识库维护者',
     desc: '个人笔记、学习记录和思考总结',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/你的GitHub用户名' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/你的Twitter用户名' },
+      { type: 'github', icon: 'github', link: 'https://github.com/Gakusara' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/Grekura' },
     ],
-    nameAliases: ['Karasu', '你的别名'], // 替换为你的可能别名
-    emailAliases: ['你的邮箱地址'],
+    nameAliases: ['Karasu', 'Gakusara','がくさら','烏世'], // 替换为你的可能别名
+    emailAliases: [''],
+  },
+  {
+    name: '絢香猫',
+    avatar: '',
+    username: 'nekomeowww',
+    title: 'Nólëbase 原始创作者',
+    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
+    links: [
+      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+    ],
+    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
+    emailAliases: ['neko@ayaka.moe'],
+  },
+  {
+    name: '絢香音',
+    avatar: '',
+    username: 'LittleSound',
+    title: 'Nólëbase 原始创作者',
+    desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
+    links: [
+      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
+    ],
+    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
+    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
